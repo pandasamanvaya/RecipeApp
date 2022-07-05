3cc92ba399f95794c6d38ce7fa8c100e2ddb445d
Samanvaya Panda <samanvayapa@Samanvayas-MacBook-Pro.local>
2022-07-05 19:20:07 +0530

    fetching items

---
+++ src/components/Item.js
@@ -0,0 +1,6 @@
+export default function Item({item}){
+    console.log(item);
+    return <>
+        <p>Item</p>
+    </>
+}
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,16 @@
-
-import React from "react"
-import { ItemContext } from "./Context"
+import React from "react";
+import { ItemContext } from "./Context";
+import Item from "./Item";
 
 export default function ItemList(){
-    let ctx = React.useContext(ItemContext)
-    console.log(ctx)
-    return <>
-        <h2>ItemList</h2>
-    </>
+    let ctx = React.useContext(ItemContext);
+    console.log("Here");
+    console.log(ctx);
+    return <div className="container">
+         <div className="row">
+            {
+                ctx.items.map(item => <Item item={item}/>)
+            }
+            </div>
+    </div>
 }
